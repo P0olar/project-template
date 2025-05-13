404be638e72106ed5652a0fdbea97e83568a78ef
P0olar <santosvictorfernando20@gmail.com>
2025-05-13 18:28:17 -0300

    update

---
+++ projetc-template/src/components/Produtos.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import produtos from '../assets/data/Produtos'
+
+const Produtos = () => {
+  return (
+    <div>Produtos</div>
+  )
+}
+
+export default Produtos
--- projetc-template/src/pages/Home.jsx
+++ projetc-template/src/pages/Home.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import Categoria from '../components/Categorias'
+import Produtos from '../components/Produtos'
 
 const Home = () => {
 
   return (
-    <>
-      {/*/<Categoria />*/}
+    <div className='home'>
+      <Categoria />
+
+      <Produtos />
+
 
       
-    </>
+    </div>
   )
 }
 
