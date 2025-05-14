67259000eed90f3f177b802a55d216b3cc77ee2d
P0olar <santosvictorfernando20@gmail.com>
2025-05-13 23:50:21 -0300

    update

--- projetc-template/src/assets/data/Produtos.js
+++ projetc-template/src/assets/data/Produtos.js
@@ -2,18 +2,35 @@ let produtos = [
     {
         id: 1,
         titulo: 'Produto Teste com 60un',
-        SKU: 'P001',
+        sku: 'P001',
         preco: 23.90,
         sobre: 'sukdcvosdvpisdznvpcszdjopvjspdvjpsdvj[p',
         imgProd: [
-            {
-            0: 'https://cimedremedios.com.br/wp-content/uploads/2020/03/Lavitan_Cafeina-1-1.png',
-            1: 'https://cimedremedios.com.br/wp-content/uploads/2020/03/Lavitan_Shake_chocolate_front.png',
-            2: 'https://cimedremedios.com.br/wp-content/uploads/2020/03/Lavitan_Carbogel_sache.png',
-            3: 'https://cimedremedios.com.br/wp-content/uploads/2021/08/103464.png',
-            4: 'https://cimedremedios.com.br/wp-content/uploads/2025/04/09-LAVITAN-SUPER-FORMULA-CABELOS-E-UNHAS__.png',
-            5: 'https://cimedremedios.com.br/wp-content/uploads/2025/04/25-LAVITAN-CARBOGEL-LIMAO_.png,'
-        }],
+            
+            'https://cimedremedios.com.br/wp-content/uploads/2020/03/Lavitan_Cafeina-1-1.png',
+            'https://cimedremedios.com.br/wp-content/uploads/2020/03/Lavitan_Shake_chocolate_front.png',
+            'https://cimedremedios.com.br/wp-content/uploads/2020/03/Lavitan_Carbogel_sache.png',
+            'https://cimedremedios.com.br/wp-content/uploads/2021/08/103464.png',
+            'https://cimedremedios.com.br/wp-content/uploads/2025/04/09-LAVITAN-SUPER-FORMULA-CABELOS-E-UNHAS__.png',
+            'https://cimedremedios.com.br/wp-content/uploads/2025/04/25-LAVITAN-CARBOGEL-LIMAO_.png,'
+        ],
+        marca: 'Cimed'
+    },
+    {
+        id: 1,
+        titulo: 'Produto Teste com 60un',
+        sku: 'P001',
+        preco: 23.90,
+        sobre: 'sukdcvosdvpisdznvpcszdjopvjspdvjpsdvj[p',
+        imgProd: [
+            
+            'https://cimedremedios.com.br/wp-content/uploads/2020/03/Lavitan_Cafeina-1-1.png',
+            'https://cimedremedios.com.br/wp-content/uploads/2020/03/Lavitan_Shake_chocolate_front.png',
+            'https://cimedremedios.com.br/wp-content/uploads/2020/03/Lavitan_Carbogel_sache.png',
+            'https://cimedremedios.com.br/wp-content/uploads/2021/08/103464.png',
+            'https://cimedremedios.com.br/wp-content/uploads/2025/04/09-LAVITAN-SUPER-FORMULA-CABELOS-E-UNHAS__.png',
+            'https://cimedremedios.com.br/wp-content/uploads/2025/04/25-LAVITAN-CARBOGEL-LIMAO_.png,'
+        ],
         marca: 'Cimed'
     }
 ];
--- projetc-template/src/components/Produtos.jsx
+++ projetc-template/src/components/Produtos.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import produtos from '../assets/data/Produtos'
 
+
 const Produtos = () => {
-  return (
-    <div>Produtos</div>
-  )
+    return (
+        <div>
+            <p>Produtos</p>
+            <div style={{ display: 'flex', gap: '20px', flexWrap: 'wrap' }}>
+                {produtos.map((produtos, index) => (
+                    <div key={index} style={{ border: '1px solid #ccc', padding: '10px' }}>
+                        <img src={produtos.imgProd[0]} alt={produtos.nomeCategoria} style={{ width: 'auto', height: '180px' }} />
+                        <h3>{produtos.titulo}</h3>
+                        <p>R$ {produtos.preco.toFixed(2).replace('.', ',')}</p>
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
 }
 
 export default Produtos
